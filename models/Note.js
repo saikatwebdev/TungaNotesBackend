29697e0f391cb3cae85fc387a0a6c8e01cd3d441
Saikat Tunga <saikattunga56789@gmail.com>
2025-10-21 00:54:36 +0530

    added created time

--- models/Note.js
+++ models/Note.js
@@ -17,6 +17,10 @@ const noteSchema = new mongoose.Schema(
       ref: 'User',
       required: true,
     },
+    createdAt:{
+      type:Date,
+      default: Date.now,
+    }
   },
   { timestamps: true }
 );
